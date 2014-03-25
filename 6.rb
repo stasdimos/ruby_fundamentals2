bc45787d593a5811d5064dab2bcd96b818dcee57
stasdimos <stas.dimos@icloud.com>
2014-03-25 16:47:14 -0400

    6update

--- 6.rb
+++ 6.rb
@@ -1,2 +1,24 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+puts grocery_list # Put List
+
+grocery_list << "rice" # Add Rice
+
+puts grocery_list # Put List Again
+
+puts grocery_list.length # How Long is List
+
+if grocery_list.include? ('Banana')
+	then
+		puts "You don't need to pick up Bananas"
+	else
+		puts "You need to pick up Bananas"
+end
+
+puts grocery_list[1]
+
+puts grocery_list.sort
+
+grocery_list.delete_at(3)
 puts grocery_list
+
