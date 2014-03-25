cbde4816c7af869b3e313e34bd987f87cd63eca3
stasdimos <stas.dimos@icloud.com>
2014-03-25 17:15:54 -0400

    7 and udpdate

--- 6.rb
+++ 6.rb
@@ -1,13 +1,19 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-puts grocery_list  # List with asteriks
+# 1 > List with Asteriks
+puts grocery_list 
+
+# Question 2 Put List Again
 
 grocery_list << "rice" # Add Rice
 
-puts grocery_list # Put List Again
 
+puts grocery_list 
+
+#Question 3
 puts "You need to pick up #{grocery_list.length} items"
 
+#Question 4
 if grocery_list.include? ('Banana')
 	then
 		puts "You don't need to pick up Bananas"
